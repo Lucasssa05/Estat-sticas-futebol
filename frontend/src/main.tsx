d00cf870a6eb13dd22392c97a3f5b5d73ad4c04f
Lucasssa05 <lucas.s.almeida@aln.senaicimatec.edu.br>
2025-04-09 20:48:55 -0300

    add: PlayerResults page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PlayerFinder from './pages/PlayerFinder.tsx';
+import PlayerResults from './pages/PlayerResults.tsx';
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
       <Route path='/playerfinder' element ={<PlayerFinder />} />
+      <Route path='/results/:searchTerm' element ={<PlayerResults />} />
       </Routes>
     </Router>
     
